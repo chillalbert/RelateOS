0f3171d55fdca9f315a3aa7ef8dfbfabaa487208
chillalbert <88564326+chillalbert@users.noreply.github.com>
2026-06-05 12:49:08 -0700

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -11,6 +11,7 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 googleProvider.addScope("https://www.googleapis.com/auth/calendar.readonly");
+googleProvider.addScope("https://www.googleapis.com/auth/contacts.readonly");
 
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
 export const storage = getStorage(app);
