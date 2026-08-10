7280cc5dfb38ec3a8cec036f3d4db2a314797a49
chillalbert <88564326+chillalbert@users.noreply.github.com>
2026-08-10 15:05:23 -0700

    Remove push notifications registration
    Removed push notifications registration from main entry point.

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { registerPushNotifications } from './pushService';
-
-// Boot up native device Background/Push Notifications system scaffolding
-registerPushNotifications();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
